314f4ab721759e058cc9efe2a44f131c06ef6cef
shamim1620 <mdshamimahmed2025@gmail.com>
2022-02-28 23:33:37 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  const handleOnClick = () => {
+    console.log("clicked")
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Matching Game</h3>
+
+      <div className='game-container'>
+        <div className="grid-container">
+          <div onClick={handleOnClick} className="grid-item">1</div>
+          <div onClick={handleOnClick} className="grid-item">2</div>
+          <div onClick={handleOnClick} className="grid-item">3</div>
+          <div onClick={handleOnClick} className="grid-item">4</div>
+          <div onClick={handleOnClick} className="grid-item">5</div>
+          <div onClick={handleOnClick} className="grid-item">6</div>
+          <div onClick={handleOnClick} className="grid-item">7</div>
+          <div onClick={handleOnClick} className="grid-item">8</div>
+          <div onClick={handleOnClick} className="grid-item">9</div>
+          <div onClick={handleOnClick} className="grid-item">7</div>
+          <div onClick={handleOnClick} className="grid-item">8</div>
+          <div onClick={handleOnClick} className="grid-item">9</div>
+        </div>
+      </div>
+
     </div>
   );
 }
