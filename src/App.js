ffd071578b9e6562a062b26746c752b882556a9a
shamim1620 <mdshamimahmed2025@gmail.com>
2022-03-01 15:02:18 +0600

    reload and move count implement

--- src/App.js
+++ src/App.js
@@ -1,28 +1,76 @@
 
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faRecycle, faStar, } from '@fortawesome/free-solid-svg-icons'
+import { useEffect, useState } from 'react';
 import './App.css';
 
+
+
+
+
 function App() {
+  const [data, setData] = useState([]);
+  const [count, setCount] = useState(0);
+  const [move, setMove] = useState(0);
+
+  useEffect(() => {
+    setMove(0);
+    fetch('data.json')
+      .then(res => res.json())
+      .then(data => {
+        function shuffle(array) {
+          let currentIndex = array.length, randomIndex;
+
+          // While there remain elements to shuffle...
+          while (currentIndex != 0) {
+
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+
+            // And swap it with the current element.
+            [array[currentIndex], array[randomIndex]] = [
+              array[randomIndex], array[currentIndex]];
+          }
+
+          return array;
+        }
+        setData(shuffle(data))
+      })
+
+  }, [count])
+
   const handleOnClick = () => {
     console.log("clicked")
   }
   return (
     <div className="App">
       <h3>Matching Game</h3>
+      <div className='game-container'>
+        <div className='icon-section'>
+          <FontAwesomeIcon icon={faStar} />
+          <FontAwesomeIcon icon={faStar} />
+          <FontAwesomeIcon icon={faStar} />
+        </div>
+        <div className='icon-section'>
+
+        </div>
+        <div className='icon-section' >
+          <h4>{Math.floor(move / 2)} Moves</h4>
+        </div>
+        <div className='icon-section'>
+          <button className='reload-btn' onClick={() => setCount(count + 1)}><FontAwesomeIcon icon={faRecycle} /></button>
+        </div>
+      </div>
 
       <div className='game-container'>
         <div className="grid-container">
-          <div onClick={handleOnClick} className="grid-item">1</div>
-          <div onClick={handleOnClick} className="grid-item">2</div>
-          <div onClick={handleOnClick} className="grid-item">3</div>
-          <div onClick={handleOnClick} className="grid-item">4</div>
-          <div onClick={handleOnClick} className="grid-item">5</div>
-          <div onClick={handleOnClick} className="grid-item">6</div>
-          <div onClick={handleOnClick} className="grid-item">7</div>
-          <div onClick={handleOnClick} className="grid-item">8</div>
-          <div onClick={handleOnClick} className="grid-item">9</div>
-          <div onClick={handleOnClick} className="grid-item">7</div>
-          <div onClick={handleOnClick} className="grid-item">8</div>
-          <div onClick={handleOnClick} className="grid-item">9</div>
+          {
+            data.map(item => <div onClick={() => setMove(move + 1)} className="grid-item">{item.id}</div>)
+          }
+
+
         </div>
       </div>
 
